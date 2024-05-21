9a75b9b651b441640aac76078874571ac4c70c0e
Nguyễn Quang Đông <101628525+Quangdong2k3@users.noreply.github.com>
2024-05-21 08:33:27 +0700

    update class DATAdatabaseSQLite

--- ManageStudent/app/src/main/java/com/tlu/managestudent/MainActivity.kt
+++ ManageStudent/app/src/main/java/com/tlu/managestudent/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.tlu.managestudent
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var id  :EditText
+    private  lateinit var name:EditText
+    private  lateinit var siso:EditText
+    private lateinit var btnAdd:Button
+    private lateinit var btnUpdate:Button
+    private lateinit var btnDelete:Button
+    private lateinit var btnSelect:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        init()
+
+
+        
+    }
+
+
+    private fun init(){
+        id = findViewById(R.id.txt_id)
+        name = findViewById(R.id.txt_class)
+        siso = findViewById(R.id.txt_amount_student)
+
+        btnAdd=findViewById(R.id.btn_insert)
+
+        btnDelete = findViewById(R.id.btn_delete)
+
+        btnSelect = findViewById(R.id.btn_select)
+
+        btnUpdate=findViewById(R.id.btn_update)
     }
 }
--- ManageStudent/app/src/main/java/com/tlu/managestudent/MyDatabase.kt
+++ ManageStudent/app/src/main/java/com/tlu/managestudent/MyDatabase.kt
@@ -1,4 +1,97 @@
 package com.tlu.managestudent
 
-class MyDatabase {
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+@RequiresApi(Build.VERSION_CODES.P)
+class MyDatabase(
+    context: Context?, factory: SQLiteDatabase.CursorFactory?,
+    name: String?,
+    version: Int
+
+) : SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
+
+
+    val createTableClass: String =
+        "CREATE TABLE $TABLE_NAME ($ID_COL TEXT PRIMARY KEY NOT NULL, $NAME_COl TEXT, $SISO_COL INTEGER)"
+
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(createTableClass)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun insertData(c: LopHoc) {
+        val contentValues = ContentValues()
+        contentValues.put(ID_COL, c.maLop)
+        contentValues.put(NAME_COl, c.tenLop)
+        contentValues.put(SISO_COL, c.numberStudent)
+
+
+        val db = this.writableDatabase
+
+        db.insert(TABLE_NAME, null, contentValues)
+
+
+        db.close()
+    }
+
+    @SuppressLint("Range")
+    fun getAllData(): ArrayList<LopHoc> {
+
+        val list: ArrayList<LopHoc> = arrayListOf()
+
+        val db = this.readableDatabase
+
+        val cursor: Cursor = db.rawQuery("SELECT * FROM $TABLE_NAME", null)
+        if (cursor.moveToFirst()) {
+            do {
+                list.add(
+                    LopHoc(
+                        cursor.getString(cursor.getColumnIndex(ID_COL)).toString(),
+                        cursor.getString(
+                            cursor.getColumnIndex(
+                                NAME_COl
+                            )
+                        ),
+                        cursor.getString(cursor.getColumnIndex(SISO_COL)).toInt()
+                    )
+                )
+            } while (cursor.moveToNext())
+        }
+
+        return list
+    }
+
+
+    companion object {
+        // here we have defined variables for our database
+
+        // below is variable for database name
+        private val DATABASE_NAME = "LopHoc.db"
+
+        // below is the variable for database version
+        private val DATABASE_VERSION = 2
+
+        // below is the variable for table name
+        val TABLE_NAME = "lophoc_table"
+
+        // below is the variable for id column
+        val ID_COL = "id"
+
+        // below is the variable for name column
+        val NAME_COl = "tenLop"
+
+        // below is the variable for age column
+        val SISO_COL = "numberStudent"
+    }
 }
